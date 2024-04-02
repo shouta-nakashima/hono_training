528234644928456910147a613e36fe5b32642654
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2024-04-02 20:48:40 +0900

    feat: 取得したデータをview に表示

--- src/pages/blogs.tsx
+++ src/pages/blogs.tsx
@@ -1,17 +1,30 @@
 import {Layout} from './Layout'
 import {link, header, base, footer } from "./style";
 import {Style} from 'hono/css'
+import type {BlogList} from '../types/blogs'
 
-export const Blogs = () => {
+type Props = {
+  blogs: BlogList
+}
+
+export const Blogs = ({blogs}: Props) => {
+
   return (
     <Layout title="blogs page" style={<Style/>}>
       <main className={base}>
         <header className={header}>
-          <h2>top page</h2>
+          <h2>blogs page</h2>
           <a className={link} href='http://localhost:3000/pages'>トップページ</a>
         </header>
         <div style={{flexGrow: 1}}>
-          <p>Welcome to Blogs page!</p>
+          <ul>
+            {blogs.map(blog => (
+              <li key={blog.id}>
+                <h3>{blog.title}</h3>
+                <p>{blog.body}</p>
+              </li>
+            ))}
+          </ul>
         </div>
         <footer className={footer}>
           <p>© 2024 Hono Example</p>
